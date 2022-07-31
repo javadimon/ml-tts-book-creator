27139ffcac69ef0a76066e0f5a858f011d405c68
Dmitry Zubanoff <zubanov@gmail.com>
2022-07-31 18:47:17 +0300

    Added WebClient

--- src/main/java/com/zubanoff/ml/tts/book/creator/service/BookCreator.java
+++ src/main/java/com/zubanoff/ml/tts/book/creator/service/BookCreator.java
@@ -12,17 +12,15 @@ public class BookCreator {
 
     private final Converter converter;
 
-//    @PostConstruct
-//    public void init(){
-//        createBook();
-//    }
-
     public void createBook() {
-        // TODO
-        log.info("Try book create");
-        converter.convert("Если подъезжать к Ланну с юга, то с холмов его видно из далека. А слышно еще дальше. " +
-                "Колокола Ланна известны на весь мир, что чтит Истинного Бога и Мать Церковь. " +
-                "Издали город кажется огромным и прекрасным. Чистым и белым.");
-        log.info("Book created");
+        // TODO SPLIT BOOK TO CHAPTERS
+        // TODO SPLIT CHAPTERS TO CHUNKS
+        // TODO SEND CHUNKS TO CONVERTER
+        // TODO MERGE MP3 CHUNKS TO MP3 CHAPTERS
+//        log.info("Try book create");
+//        converter.convert("Если подъезжать к Ланну с юга, то с холмов его видно из далека. А слышно еще дальше. " +
+//                "Колокола Ланна известны на весь мир, что чтит Истинного Бога и Мать Церковь. " +
+//                "Издали город кажется огромным и прекрасным. Чистым и белым.");
+//        log.info("Book created");
     }
 }
