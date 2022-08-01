a5771e48f3c2cc912ed59de4aef70391da3d08de
Dmitry Zubanoff <zubanov@gmail.com>
2022-08-01 18:48:48 +0300

    Added book chapter splitter

--- src/main/java/com/zubanoff/ml/tts/book/creator/service/BookCreator.java
+++ src/main/java/com/zubanoff/ml/tts/book/creator/service/BookCreator.java
@@ -2,8 +2,16 @@ package com.zubanoff.ml.tts.book.creator.service;
 
 import com.zubanoff.ml.tts.book.creator.service.converter.Converter;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 @RequiredArgsConstructor
 @Service
@@ -11,8 +19,11 @@ import org.springframework.stereotype.Service;
 public class BookCreator {
 
     private final Converter converter;
+    private static final String CHAPTER_SPLITTER = "глава";
+    private static final String SHORT_NEW_LINE = "\n";
+    private static final String FULL_NEW_LINE = "\r\n";
 
-    public void createBook() {
+    public void createBook(Path bookPath) {
         // TODO SPLIT BOOK TO CHAPTERS
         // TODO SPLIT CHAPTERS TO CHUNKS
         // TODO SEND CHUNKS TO CONVERTER
@@ -22,5 +33,33 @@ public class BookCreator {
 //                "Колокола Ланна известны на весь мир, что чтит Истинного Бога и Мать Церковь. " +
 //                "Издали город кажется огромным и прекрасным. Чистым и белым.");
 //        log.info("Book created");
+    }
+
+    @SneakyThrows
+    public Map<Integer, List<String>> splitBookToChapters(Path bookPath) {
+        Map<Integer, List<String>> chapters = new TreeMap<>();
+        List<String> lines = Files.readAllLines(bookPath);
+        int chapterNumber = 1;
+        int count = 0;
+        for (String line : lines) {
+            if (line.toLowerCase().startsWith(CHAPTER_SPLITTER) && count == 0) {
+                if (lines.get(count + 1).startsWith(SHORT_NEW_LINE) || lines.get(count + 1).startsWith(FULL_NEW_LINE)) {
+                    chapters.put(chapterNumber, List.of(line));
+                    chapterNumber++;
+                }
+            } else if (line.toLowerCase().startsWith(CHAPTER_SPLITTER) && count > 0 && count + 1 < lines.size()) {
+                if ((lines.get(count - 1).startsWith(SHORT_NEW_LINE) || lines.get(count - 1).startsWith(FULL_NEW_LINE)) &&
+                        (lines.get(count + 1).startsWith(SHORT_NEW_LINE) || lines.get(count + 1).startsWith(FULL_NEW_LINE))) {
+                    chapters.put(chapterNumber, List.of(line));
+                    chapterNumber++;
+                }
+            } else {
+                // TODO
+            }
+
+            count++;
+        }
+
+        return chapters;
     }
 }
