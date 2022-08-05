73ad9408986ac3e3bff8d9a67417edb96cccf1a1
Dmitry Zubanoff <zubanov@gmail.com>
2022-08-05 12:20:46 +0300

    Added book controller, repository, model etc.

--- src/main/java/com/zubanoff/ml/tts/book/creator/service/BookCreatorService.java
+++ src/main/java/com/zubanoff/ml/tts/book/creator/service/BookCreatorService.java
@@ -39,7 +39,7 @@ public class BookCreatorService {
         List<TreeMap<String, String>> chunks = splitChaptersToChunks(chapters);
         for(TreeMap<String, String> chunk : chunks){
             for(Map.Entry<String, String> entry : chunk.entrySet()){
-                if(entry.getKey().startsWith("Chapter 009")){
+                if(entry.getKey().startsWith("Chapter 010")){
                     log.info("Try convert chapter {}, length {}", entry.getKey(), entry.getValue().length());
                     Runnable runnable = () -> {
                         converter.convert(entry.getKey(), entry.getValue());
